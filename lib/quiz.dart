2d3e126fe8bf861df5bf49c151a459837334e65e
xcheche <chekwubechukwu23@gmail.com>
2023-09-28 12:48:46 +0100

    in progress

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'start_screen.dart'; // Correct the import statement for 'start_screen.dart'
 import 'questions_screen.dart'; // Correct the import statement for 'questions_screen.dart'
+import 'package:adv_basics/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -11,6 +12,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen'; // Correct the variable name
 
   void switchScreen() {
@@ -19,12 +21,23 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidget =
         activeScreen == 'start-screen' // Correct the variable name
             ? StartScreen(switchScreen)
-            : const QuestionScreen();
+            : QuestionScreen(onSelectAnswer: chooseAnswer);
     return MaterialApp(
       home: Scaffold(
         body: Container(
