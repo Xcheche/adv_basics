eef333832a6649b7f45394df89c615fe602f82d4
xcheche <chekwubechukwu23@gmail.com>
2023-09-28 13:29:41 +0100

    result screen added

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'start_screen.dart'; // Correct the import statement for 'start_screen.dart'
 import 'questions_screen.dart'; // Correct the import statement for 'questions_screen.dart'
 import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/result_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -27,17 +28,22 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'result-screen';
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenWidget =
+    var screenWidget =
         activeScreen == 'start-screen' // Correct the variable name
             ? StartScreen(switchScreen)
             : QuestionScreen(onSelectAnswer: chooseAnswer);
+
+    if (activeScreen == 'result-screen') {
+      screenWidget = const ResultScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
