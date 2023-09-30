71237c688b7e329df69e8f69dea523957fd35063
xcheche <chekwubechukwu23@gmail.com>
2023-09-30 20:47:19 +0100

    result_screen.dart updated

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,10 +1,27 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({Key? key, required this.choosenAnswers})
       : super(key: key); // Fix the parameter name
 
-  final List<String> choosenAnswers; // Fix the variable name
+  final List<String> choosenAnswers;
+  // Fix the variable name
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> Summary = [];
+
+    for (var i = 0; i < choosenAnswers.length; i++) {
+      Summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[1].answers[0],
+        'user_answer': choosenAnswers[i]
+      });
+    }
+
+    return Summary;
+  }
 
   @override
   Widget build(BuildContext context) {
